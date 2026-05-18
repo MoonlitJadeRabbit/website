f4cd8d226a38470ee65e7163cce093a3e54e04d8
Junye Zhou <junye.zhou@mail.utoronto.ca>
2026-05-17 23:12:01 -0400

    Enable Web3Forms contact delivery.

--- contact-config.js
+++ contact-config.js
@@ -1,6 +1,6 @@
 // Get a free access key at https://web3forms.com (use Gmail if U of T blocks signup mail).
 // Paste the key between the quotes below, save, and push to GitHub.
 window.SITE_CONTACT = {
-  web3formsAccessKey: "",
+  web3formsAccessKey: "73c73ce9-9596-419c-a114-d99ff47cba30",
   displayEmail: "junye.zhou@mail.utoronto.ca",
 };
